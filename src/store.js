e6559a91996f9821f4a5724c516b9a5b13b03454
TonmoyZohani <tonmoyzohani@gmail.com>
2024-02-21 17:25:17 +0600

    create store done

--- src/store.js
+++ src/store.js
@@ -16,7 +16,12 @@ const reducer = (state = initialState, action) => {
       if (state.loan > 0) {
         return state;
       }
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -31,6 +36,12 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer);
 
-store.dispatch({type:"account/deposit",payload:500});
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+store.dispatch({type:"account/payLoan"});
 
-console.log('Hello World');
+console.log(store.getState());

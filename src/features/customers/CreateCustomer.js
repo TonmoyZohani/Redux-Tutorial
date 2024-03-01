c82464abc21599dfa64b4819b5dac50ce3e01855
TonmoyZohani <tonmoyzohani@gmail.com>
2024-03-01 23:09:47 +0600

    customer silce done

--- src/features/customers/CreateCustomer.js
+++ src/features/customers/CreateCustomer.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createCustomer } from "./customerSlice";
 
 function Customer() {
@@ -7,6 +7,10 @@ function Customer() {
   const [nationalId, setNationalId] = useState("");
 
   const dispatch = useDispatch();
+
+  const customer = useSelector((store)=>store.customer);
+
+  console.log(customer);
 
   function handleClick() {
     if (!fullName || !nationalId) return;
